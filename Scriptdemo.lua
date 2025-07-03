7e92c9fb139ca450eda05b11f6a1395be0f479c8
linhhehe <dang566111@gmail.com>
2025-07-03 16:51:12 +0700

    Add files via upload

--- Scriptdemo.lua
+++ Scriptdemo.lua
@@ -30,18 +30,157 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
       })
       
 ------ Tab
-     local Tab1o = MakeTab({Name = "Script bú lồn"})
+     local Tab1o = MakeTab({Name = "Script Farm"})
      
 ------- BUTTON
     
     AddButton(Tab1o, {
-     Name = "Redz Hub nè mấy thằng cặc",
+Name = "Redz Hub nè mấy thằng cặc", 
     Callback = function()
 	  local Settings = {
-  JoinTeam = "Pirates"; -- Pirates/Marines
-  Translator = true; -- true/false
+    JoinTeam = "Pirates"; -- Pirates / Marines
+    Translator = true;   -- true / false
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
-  end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau"))(Settings)
+end
   })
+  
+AddButton(Tab2o, {
+     Name = "aotu auti nè mấy thằng cặc",
+    Callback = function()
+	  local Settings = {
+	  repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Team = "Pirates"
+getgenv().Config = {
+    ["Safe Health"] = {30},
+    ["Custom Y Run"] = {
+        Enabled = true,
+        ["Y Run"] = 5000
+    },
+    ["Hunt Method"] = {
+        ["Use Move Predict"] = false,
+        ["Hit and Run"] = false,
+        ["Aimbot"] = true,
+        ["ESP Player"] = true,
+        ["Max Attack Time"] = 60
+    },
+    ["Stats"] = {
+        ["Enable"] = false,
+        ["Reset Stats"] = false,
+        ["Point"] = {
+            ["Points per lift"] = nil,
+            ["Melee"] = nil,
+            ["Defence"] = nil,
+            ["Sword"] = nil,
+            ["Gun"] = nil,
+            ["Devil Fruit"] = nil
+        }
+    },
+    ["Shop"] = {
+        ["Random Fruit"] = false,
+        ["Store Fruit"] = true,
+        ["Zoro Sword"] = false
+    },
+    ["Theme"] = {
+    Name = "Hutao",
+    UIUrl = "",
+    Custom = {
+        ["Enable"] = false,
+        ["title_color"] = Color3.fromRGB(255, 221, 252),
+        ["titleback_color"] = Color3.fromRGB(169, 20, 255),
+        ["list_color"] = Color3.fromRGB(255, 221, 252),
+        ["liststroke_color"] = Color3.fromRGB(151, 123, 207),
+        ["button_color"] = Color3.fromRGB(255, 221, 252),
+        ["title_font"] = Enum.Font.FredokaOne,
+        ["text_font"] = Enum.Font.Gotham,
+        ["title_size"] = 40,
+        ["text_size"] = 24
+        }
+    },
+    ["Setting"] = {
+        ["World"] = 3,
+        ["White Screen"] = false,
+        ["Fast Delay"] = 0.5,
+        ["Tween Speed"] = 350,
+        ["FPS BOOSTER"] = false,
+        ["Url"] = "",
+        ["Chat"] = {
+            Enabled = false,
+            Wait = 350,
+            Massage = {""}
+        }
+    },
+    ["Skip"] = {
+        ["Avoid V4"] = false,
+        ["Fruit"] = {
+            ["Enabled"] = true,
+            ["Avoid Fruit"] = {
+                "Portal-Portal",
+                "Kitsune-Kitsune"
+            }
+        }
+    },
+    ["Spam All Skill On V4"] = {
+        Enabled = true,
+        ["Weapons"] = {"Melee", "Sword", "Gun", "Blox Fruit"}
+    },
+    ["Items"] = {
+        ["Use"] = {"Melee", "Sword", "Gun", "Blox Fruit"},
+        ["Melee"] = {
+            Enable = true,
+            Delay = 0.4,
+            Skills = {
+                Z = {Enable = true, HoldTime = 0.3},
+                X = {Enable = true, HoldTime = 0.2},
+                C = {Enable = true, HoldTime = 0.5}
+            }
+        },
+        ["Sword"] = {
+            Enable = true,
+            Delay = 0.5,
+            Skills = {
+                Z = {Enable = true, HoldTime = 1},
+                X = {Enable = true, HoldTime = 0}
+            }
+        },
+        ["Gun"] = {
+            Enable = false,
+            Delay = 0.3,
+            Skills = {
+                Z = {Enable = true, HoldTime = 0.1},
+                X = {Enable = true, HoldTime = 0.1}
+            }
+        },
+        ["Blox Fruit"] = {
+            Enable = false,
+            Delay = 0.4,
+            Skills = {
+                Z = {Enable = true, HoldTime = 0.1},
+                X = {Enable = true, HoldTime = 0.1},
+                C = {Enable = true, HoldTime = 0.15},
+                V = {Enable = true, HoldTime = 0.2},
+                F = {Enable = true, HoldTime = 0.1}
+            }
+        }
+    }
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e80e11cce2599152482a2d3e7d6b99d7.lua"))()
+end
+  })
+  
+AddButton(Tab3o, {
+     Name = "Astral Hub nè mấy thằng cặc",
+    Callback = function()
+	  local Settings = {
+	  loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))() 
+	  end
+  })
+  
+	  AddButton(Tab4o, {
+     Name = "Fetching Hub nè mấy thằng cặc",
+    Callback = function()
+	  local Settings = {
+	  loadstring(game:HttpGet("https://github.com/MerrySubs4t/Softwork/blob/main/Fetching/dll?raw=true"))()
+	end
+  })  	  
